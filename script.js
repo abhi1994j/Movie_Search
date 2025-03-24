77c772221607673f3a056b3c6dcb10dcb0a19fc3
abhi1994j <abhibaban4@gmail.com>
2025-03-24 18:04:14 +0530

    ui cave

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
 async function loadMovies(searchTerm){
-    const URL = `http://www.omdbapi.com/?s=${searchTerm}&page=1&i=tt3896198&apikey=2d760e1`
+    const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&i=tt3896198&apikey=2d760e1`
     const res = await fetch(`${URL}`);
     const data = await res.json();
     console.log(data.Search);
